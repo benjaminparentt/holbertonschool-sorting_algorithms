9a4f7f1c432f32df5f1bd88ba3b79912f318bff8
cel3788 cela <7576@holbertonstudents.com>
2024-01-19 21:13:03 +0000

    Adding all files to repository

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -8,19 +8,19 @@
  */
 void nodes_swap(listint_t **list, listint_t *first, listint_t *second)
 {
-    if (!first->prev)   /* at the first node */
-        *list = second;
-    else
-        first->prev->next = second;
+	if (!first->prev)   /* at the first node */
+		*list = second;
+	else
+		first->prev->next = second;
 
-    second->prev = first->prev;
+	second->prev = first->prev;
 
-    if (second->next)   /* at the last node */
-        second->next->prev = first;
+	if (second->next)   /* at the last node */
+		second->next->prev = first;
 
-    first->prev = second;
-    first->next = second->next;
-    second->next = first;
+	first->prev = second;
+	first->next = second->next;
+	second->next = first;
 }
 
 /**
@@ -29,43 +29,43 @@ void nodes_swap(listint_t **list, listint_t *first, listint_t *second)
  */
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *future;
-    int swapped = 1;
+	listint_t *future;
+	int swapped = 1;
 
-    if (!list || !(*list) || !(*list)->next)  /* only one node */
-        return;
+	if (!list || !(*list) || !(*list)->next)  /* only one node */
+		return;
 
-    future = *list;
-    while (swapped)
-    {
-        swapped = 0;
-        while (future->next)    /* traverse forward */
-        {
-            if (future->n > future->next->n)
-            {
-                nodes_swap(list, future, future->next);
-                print_list(*list);
-                swapped = 1;
-                continue;
-            }
-            future = future->next;
-        }
+	future = *list;
+	while (swapped)
+	{
+		swapped = 0;
+		while (future->next)    /* traverse forward */
+		{
+			if (future->n > future->next->n)
+			{
+				nodes_swap(list, future, future->next);
+				print_list(*list);
+				swapped = 1;
+				continue;
+			}
+			future = future->next;
+		}
 
-        if (!swapped)   /* list is sorted */
-            break;
+		if (!swapped)   /* list is sorted */
+			break;
 
-        swapped = 0;
-        while (future->prev)    /* traverse backwards */
-        {
-            if (future->n < future->prev->n)
-            {
-                nodes_swap(list, future->prev, future);
-                print_list(*list);
-                swapped = 1;
-                continue;
-            }
-            future = future->prev;
-        }
-    }
+		swapped = 0;
+		while (future->prev)    /* traverse backwards */
+		{
+			if (future->n < future->prev->n)
+			{
+				nodes_swap(list, future->prev, future);
+				print_list(*list);
+				swapped = 1;
+				continue;
+			}
+			future = future->prev;
+		}
+	}
 }
 
